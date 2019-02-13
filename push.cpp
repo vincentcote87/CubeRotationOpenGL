2c94c952f7c74e8806dc90468949cafbebe224d3
Vincent Cote <vincentcote87@gmail.com>
2019-02-13 12:56:06 -0700

    Finished both questions

--- push.cpp
+++ push.cpp
@@ -1,8 +1,3 @@
-
-/*
- * hello.c
- * This is a simple, introductory OpenGL program.
- */
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -11,6 +6,7 @@
 #include <GL/glu.h>
 
 float EYEx, EYEy = 0.0;
+float EYEz = 5.0;
 float angle = 0.0;
 float z = 0.0;
 
@@ -28,13 +24,13 @@ void display(void)
    glLoadIdentity();
 
    /* Viewing transformation */
-   gluLookAt(EYEx, EYEy, (5.0 + z),   /* Eye */
+   gluLookAt(EYEx, EYEy, (EYEz + z),   /* Eye */
              0.0, 0.0, 0.0,   /* Look at */
              0.0, 1.0, 0.0);  /* Up vector */
 
-  glTranslatef(0.0, 0.0, z);
-  glRotated(angle, 0.0, 1.0, 0.0);
-   /* Create a unit cube whose center is at (0, 0, 0) */
+   glColor3f(1.0, 0.0, 1.0);
+   glTranslatef(0.0, 0.0, z);
+   glRotated(angle, 0.0, 1.0, 0.0);
    glutWireCube(1.0);
 
    angle += 0.01;
@@ -47,7 +43,7 @@ void display(void)
    glFlush ();
 }
 
-void init (void) 
+void init (void)
 {
    /* select clearing color 	*/
    glClearColor (0.0, 0.0, 0.0, 0.0);
@@ -64,7 +60,7 @@ void reshape(int w, int h)
    glLoadIdentity();
    glFrustum(-1.0, 1.0, /* Left and right boundary */
              -1.0, 1.0, /* bottom and top boundary */
-             1.5, 20.0); /* near and far boundary */
+             1.5, 200.0); /* near and far boundary */
    glMatrixMode(GL_MODELVIEW);
 }
 
@@ -92,18 +88,20 @@ void mouse(int button, int state, int x, int y)
       EYEy = 0;
     }
   }
-  if (button == GLUT_LEFT_BUTTON) {
-    z -= 1.0;
-  }
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+  if (key == 122) {
+    z -= 0.01;
+  };
 }
 
 int main(int argc, char** argv)
 {
-  // EYEx = 5.0;
-  // EYEy = 5.0;
    glutInit(&argc, argv);
    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-   glutInitWindowSize(500, 500); 
+   glutInitWindowSize(500, 500);
    glutInitWindowPosition (100, 100);
    glutCreateWindow (argv[0]);
    init();
@@ -111,6 +109,7 @@ int main(int argc, char** argv)
    glutIdleFunc(display);
    glutReshapeFunc(reshape);
    glutMouseFunc(mouse);
+   glutKeyboardFunc(keyboard);
    glutMainLoop();
    return 0;   /* We'll never be here.*/
 }
